8b88bbbbd7d3319cd24fb5a34e3477ec31513a23
Jamie King <jamiedanielking@gmail.com>
2026-05-11 21:01:46 +0100

    added device types

--- src/lib/config/devices.types.ts
+++ src/lib/config/devices.types.ts
@@ -2,10 +2,18 @@
 // ~~~ device config types
 //
 
+// device types
+export enum DEVICE_TYPE {
+    LIGHT = "light"
+    MOTION = "motion"
+    BUTTON = "button"
+}
+
 // individual device configs
 export interface DeviceConfig {
     name: string,
-    mqtt: string
+    mqtt: string,
+    type: DEVICE_TYPE
 }
 
 // global device config
