e81ab0db3646b04927be0067c0ebf08052733904
Jamie King <git@jamiedanielk.ing>
2026-06-22 19:53:05 +0100

    added base dashboard pages

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -16,5 +16,33 @@ router.get("/", (_, res) => {
     res.render("index")
 })
 
+// devices route
+router.get("/devices", (_, res) => {
+    logger.debug("D> loading dashboard devices")
+
+    res.render("devices")
+})
+
+// actions route
+router.get("/actions", (_, res) => {
+    logger.debug("D> loading dashboard actions")
+
+    res.render("actions")
+})
+
+// settings route
+router.get("/settings", (_, res) => {
+    logger.debug("D> loading dashboard settings")
+
+    res.render("settings")
+})
+
+// about route
+router.get("/about", (_, res) => {
+    logger.debug("D> loading dashboard about")
+
+    res.render("about")
+})
+
 // export router
 export default router
