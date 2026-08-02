c7722b65281fe587fb8c6a9350b6f79e629ab16a
Jamie King <git@jamiedanielk.ing>
2026-08-02 11:04:24 +0100

    added basic alert banners

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -4,6 +4,8 @@
 
 // imports
 import { Router } from "express"
+import allDevices from "@lib:config/devices"
+import allActions from "@lib:config/actions"
 import logger from "@lib:logger"
 
 // setup router
@@ -20,14 +22,18 @@ router.get("/", (_, res) => {
 router.get("/devices", (_, res) => {
     logger.debug("D> loading dashboard devices")
 
-    res.render("devices")
+    res.render("devices", {
+        devices: allDevices
+    })
 })
 
 // actions route
 router.get("/actions", (_, res) => {
     logger.debug("D> loading dashboard actions")
 
-    res.render("actions")
+    res.render("actions", {
+        actions: allActions
+    })
 })
 
 // settings route
