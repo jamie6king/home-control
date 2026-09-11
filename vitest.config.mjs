72efaaa92c32a7eca98a9ff3a2cccdf9a6524eaa
Jamie King <git@jamiedanielk.ing>
2026-09-11 16:10:44 +0100

    improve test speed

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
     "test": {
+        "isolate": false,
+        "pool": "threads",
         "coverage": {
             "provider": "v8",
             "include": ["src/**/*.ts"],
